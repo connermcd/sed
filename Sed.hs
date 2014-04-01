2f1efb9f748ced3795b2433c890e9a8872565a45
Conner McDaniel <connermcd@gmail.com>
2014-03-31 22:57:47 -0500

    Rename input to zipper.

--- Sed.hs
+++ Sed.hs
@@ -8,10 +8,9 @@ import qualified Text.Regex as TR
 
 data SedState = SedState {
                   line         :: Int
-                , input        :: Z.Zipper T.Text
+                , zipper       :: Z.Zipper T.Text
                 , patternSpace :: T.Text
                 , holdSpace    :: T.Text
-                -- , output       :: T.Text
                 }
 
 data Command = Print
@@ -28,16 +27,16 @@ runCommands :: [Command] -> State SedState T.Text
 runCommands cs = do
     mapM_ runCommand cs
     ss <- get
-    if Z.endp $ input ss
-    then return . T.unlines . Z.toList $ input ss
+    if Z.endp $ zipper ss
+    then return . T.unlines . Z.toList $ zipper ss
     else runCommand Next >> runCommands cs
 
 runCommand :: Command -> State SedState ()
-runCommand Print = modify $ \ss -> ss { input = Z.push (patternSpace ss) (input ss) }
+runCommand Print = modify $ \ss -> ss { zipper = Z.push (patternSpace ss) (zipper ss) }
 runCommand Delete = modify $ \ss -> ss { patternSpace = T.empty }
 runCommand Next = modify $ \ss -> ss { line = line ss + 1,
-                                       input = Z.delete $ input ss,
-                                       patternSpace = Z.cursor $ input ss }
+                                       zipper = Z.delete $ zipper ss,
+                                       patternSpace = Z.cursor $ zipper ss }
 runCommand (Substitute p r _) = modify $ \ss ->
     let regex = TR.subRegex (TR.mkRegex p) (T.unpack $ patternSpace ss) r
     in ss { patternSpace = T.pack regex }
