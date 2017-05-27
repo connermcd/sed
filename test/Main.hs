e931333800775458ccc01a8aa4d4ffd07cc91747
David P. Sicilia <dpsicilia@gmail.com>
2017-05-27 13:01:28 -0400

    Add a test for laziness which now fails

--- test/Main.hs
+++ test/Main.hs
@@ -25,8 +25,23 @@ sedCase n txt cs = do
     gnuSed <- readProcess "sed" cmd txt
     return $ testCase (show cs) $ sed n cs (T.pack txt) @?= T.pack gnuSed
 
+-- Here we create an input consisting of ten valid lines plus one
+-- (final) line that will cause an error when evaluated.  We  use
+-- this input to make sure that the sed function does not consume
+-- any more input then it needs  to  produce the amount of output
+-- that we ask it for. This test uses the sed function  to  simu-
+-- late  running  `yes | sed -n 'p' | head 1` at the command line
+-- to verify that it termintaes.
+lazyCase :: IO Test
+lazyCase = do
+    let input  = replicate 10 "y" ++ [error "Non-Laziness Detected"]
+    let result = sed True "p" (T.unlines $ map T.pack $ input)
+    mapM_ print $ map T.unpack $ take 1 $ T.lines $ result
+    return $ testCase "lazyCase" (assertBool "" True)
+
 main :: IO ()
 main = do
     loud <- mapM (sedCase False text) tests
     quiet <- mapM (sedCase True text) tests
-    defaultMain [testGroup "sed" loud, testGroup "sed -n" quiet]
+    lazy <- lazyCase
+    defaultMain [testGroup "sed" loud, testGroup "sed -n" quiet, lazy]
